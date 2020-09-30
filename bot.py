114209b5e354b52fef801f5fc88133e5c2982228
afkhurana <31321265+afkhurana@users.noreply.github.com>
2020-09-30 10:59:05 -0400

    Fix error handling, forbid member from changing own nickname

--- bot.py
+++ bot.py
@@ -58,21 +58,34 @@ async def nick_user(ctx, username, nickname):
 		if member.id == user_id:
 			member_to_change = member
 	old_nickname = member_to_change.nick
-	print(f"Changing @{member_to_change}'s nickname from {old_nickname} to {nickname}")
+	print(f"Changing @{member_to_change}'s nickname from \"{old_nickname}\" to \"{nickname}\"")
 	channel_invoked = ctx.message.channel
+	if ctx.message.author == member_to_change:
+		error_text = "Hey! You can't change your own nickname!"
+		print(f"@{member_to_change} tried to change their own nickname!")
+		await channel_invoked.send(error_text)
+		return
 	try:
 		await member_to_change.edit(nick=nickname)
-		await channel.send(f"Successfully changed @{member_to_change}'s nickname from {old_nickname} to {nickname}")
+		success_text = f"Successfully changed @{member_to_change}'s nickname from \"{old_nickname}\" to \"{nickname}\""
+		print(success_text)
+		await channel_invoked.send(success_text)
 	except commands.errors.TooManyArguments:
-		await channel.send("Too many arguments! Please use the following format: nick!name @user \"New Nickname\"")
+		error_text = "Too many arguments! Please use the following format: nick!name @user \"New Nickname\""
+		print(error_text)
+		await channel_invoked.send(error_text)
 	except discord.errors.HTTPException as err:
 		if err.status == 400 and err.code == 50035:
-			if err.test == "In nick: Must be 32 or fewer in length.":
-				await channel.send("Nickname is too long! Nick must be 32 characters or fewer.")
+			if "In nick: Must be 32 or fewer in length." in err.text:
+				error_text = "Nickname is too long! Nick must be 32 characters or fewer."
+				print(error_text)
+				await channel_invoked.send(error_text)
 			else:
 				raise err
 		else:
 			raise err
+	except (discord.errors.Forbidden, commands.errors.CommandInvokeError):
+		print("[ERROR] 403 Forbidden")
 	return
 
 
